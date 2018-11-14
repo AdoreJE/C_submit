24426a86a7fbd425d9ef4839734d315e2b6e8559
thgeLove <thkim4693@naver.com>
2018-11-14 14:08:08 +0900

    2

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var multer = require('multer');
+var ip;
 var _storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, 'uploads/')
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname);
+    cb(null, file.originalname + ip);
   }
 })
 var upload = multer({ storage: _storage })
