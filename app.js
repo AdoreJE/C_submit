62dfaf37d06b479466c042062c13727c7768f6e2
thgeLove <thkim4693@naver.com>
2018-11-14 15:17:06 +0900

    ip

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var multer = require('multer');
-var writeFile = require('write');
 var fs = require('fs');
 var _storage = multer.diskStorage({
   destination: function (req, file, cb) {
